8df97f52686527944ff4c09e6af8151c0b630081
teerawat <teewat@yahoo.com>
2021-11-02 11:46:43 +1100

    completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+    dwarves.each.with_index(1) do |dwarve,i|
+      puts "#{i}. #{dwarve}"
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls )
+  planeteer_calls.collect do |planeteer_call|
+    "#{planeteer_call.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(assorted_words)
+    assorted_words.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect {|el| cheese_types.include?(el)}
 end
